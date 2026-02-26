f41bef3a27541897715889ab4303245ccfd2340a
rupesh <rpiwal02@gmail.com>
2026-02-26 23:07:54 +0530

    Add Zod dependency and implement Prisma client initialization

---
+++ app/api/products/route.ts
@@ -0,0 +1,6 @@
+export function GET(){
+    //TODO-1: fetch all the products in decreasing order using query inside try block.
+    // return the response with a status code.
+
+    //TODO-2: handle error in catch block with a status code.
+}
