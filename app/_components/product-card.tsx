07ae1f9be70ffbe036b528851cd1e94a4b21b52c
rupesh <rpiwal02@gmail.com>
2026-03-08 10:50:52 +0530

    removed un-neccessary in-line comments

--- app/_components/product-card.tsx
+++ app/_components/product-card.tsx
@@ -1,5 +1,3 @@
-
-
 import Image from "next/image";
 import { Package } from "lucide-react";
 
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,7 +5,6 @@ const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
 });
 
-// const prisma = new PrismaClient({ adapter });
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined;
